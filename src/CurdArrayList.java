d65bd6130a985e178eb2cecc4ece1f901579dc2d
Savib Pandey <savib45@gmail.com>
2022-02-02 09:11:49 +0545

    Sswitch case implemented successfully

--- src/CurdArrayList.java
+++ src/CurdArrayList.java
@@ -17,48 +17,43 @@ public class CurdArrayList {
             PreparedStatement pstm = con.prepareStatement("select * from trial_db");
             ResultSet rs = pstm.executeQuery();
             System.out.println("hello there");
+            while (rs.next()) {
+                arrayList.add(rs.getString("name"));
+                System.out.println("" + rs.getString("name"));
+
+            }
             Scanner in = new Scanner(System.in);
-            System.out.println("Entered string ");
+            System.out.println("Entered I for Insert, U for update and D for delete ");
             String s = in.nextLine();
 
             switch (s) {
                 case "I":
-                    System.out.println("Insert option");
-                    while (rs.next()) {
-                        arrayList.add(rs.getString("name"));
-                        System.out.println("" + rs.getString("name"));
+                    System.out.println("Insert option selected");
+                    System.out.println("Entered Name to add");
+                    String name = in.nextLine();
+                    arrayList.add(name);
+                    System.out.println(""+arrayList);
+                    break;
 
-                    }
+                case "U":
+                    System.out.println("update option selected");
+                    System.out.println("Entered Name to change");
+                    String updateName = in.nextLine();
+                    System.out.println("Entered index ");
+                    String updateIndex = in.nextLine();
+                    arrayList.set(Integer.parseInt(updateIndex), updateName);
+                    System.out.println("" + arrayList);
+                    break;
 
-                    System.out.println("Do you want to update(y/n)?");
-                    String check = in.nextLine();
-                    if (check.equals("y")) {
-                        System.out.println("update option");
-                        System.out.println("Entered Name to change");
-                        String updateName = in.nextLine();
-                        System.out.println("Entered index ");
-                        String updateIndex = in.nextLine();
-                        arrayList.set(Integer.parseInt(updateIndex), updateName);
-                        System.out.println(""+arrayList);
-                       
-                    }else{
-                        System.out.println(" No data updated");
-                    }
-                     
-                     System.out.println("Do you want to remove any index(y/n)?");
-                    String d = in.nextLine();
-                     if(d.equals("y")){
-                         System.out.println("Entered index ");
-                        int rIndex = in.nextInt();
-                        arrayList.remove(rIndex);
-                         System.out.println(""+arrayList);
-                    }
-
-                   
+                case "D":
+                    System.out.println("delete option selected");
+                    System.out.println("Entered index ");
+                    int rIndex = in.nextInt();
+                    arrayList.remove(rIndex);
+                    System.out.println("" + arrayList);
+                    break;
 
             }
-//                int rs = pstm.executeUpdate();
-//            pstm.setString(1, );
 
         } catch (Exception e) {
             System.out.println("error" + e.getMessage());
